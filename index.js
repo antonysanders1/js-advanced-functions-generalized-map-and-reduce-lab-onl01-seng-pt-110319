9c34dfff221db0969849c630079228b4d50c0de0
antony_sanders <antony.devon92@gmail.com>
2020-06-17 03:55:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(numArr, func) {
+    let newArr = [];
+    numArr.map(ele => {
+        newArr.push(func(ele));
+    });
+    return newArr;
+}
+
+function reduce(sourceArray, func, startingPoint) {
+    let start = (!!startingPoint) ? startingPoint : sourceArray[0];
+    let i = (!!startingPoint) ? 0 : 1;
+    for (; i < sourceArray.length; i++) {
+        start = func(sourceArray[i], start)
+    }
+    return start;
+}
